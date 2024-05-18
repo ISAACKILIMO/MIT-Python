8644f3dd84503431db801d8b9f8e34838abf20dd
ISAACKILIMO <50107673+ISAACKILIMO@users.noreply.github.com>
2024-05-18 21:53:12 +0300

    keys to list
    Signed-off-by: ISAACKILIMO <50107673+ISAACKILIMO@users.noreply.github.com>

--- More_Dictinary.py
+++ More_Dictinary.py
@@ -12,12 +12,21 @@ Created on Sat May 18 21:23:24 2024
 # dict2 = {'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
 
 # print(compressed(dict1, dict2))
-def my_dictionary(employees, defaults):
-     my_dict1={}
-     for ele in employees:
-         my_dict1[ele]=defaults
-     return my_dict1
-employees = ['Kelly', 'Emma']
-defaults = {"designation": 'Developer', "salary": 8000}
+# def my_dictionary(employees, defaults):
+#      my_dict1={}
+#      for ele in employees:
+#          my_dict1[ele]=defaults
+#      return my_dict1
+# employees = ['Kelly', 'Emma']
+# defaults = {"designation": 'Developer', "salary": 8000}
 
-print(my_dictionary(employees, defaults))
+# print(my_dictionary(employees, defaults))
+sample_dict = {
+    "name": "Kelly",
+    "age": 25,
+    "salary": 8000,
+    "city": "New york"}
+ls=[]
+for k in sample_dict.keys():
+    ls.append(k)
+print(ls)

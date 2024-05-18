f1e0c049e3cff63382ddf1357dae309acf2dd452
ISAACKILIMO <50107673+ISAACKILIMO@users.noreply.github.com>
2024-05-18 21:07:55 +0300

    Signed-off-by: ISAACKILIMO <50107673+ISAACKILIMO@users.noreply.github.com>

--- dictionary.py
+++ dictionary.py
@@ -37,14 +37,19 @@ Created on Sat May 18 08:20:12 2024
 # my_lst = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 # print(create_dict(my_lst))
 
-def create_dic(my_list):
-    my_dict = {}
-    for i in range(len(my_list)):
-        month = my_list[i]
-        for k in range(13):
-            my_dict[month] = k
-    return my_dict
+# def create_dic(my_list):
+#     my_dict = {}
+#     for i in range(len(my_list)):
+#         month = my_list[i]
+#         for k in range(13):
+#             my_dict[month] = k
+#     return my_dict
 
-my_list = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-print(create_dic(my_list))
-
+# my_list = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+# print(create_dic(my_list))
+ls=[]
+for i in range(10, 20):
+    for k in range(30,40):
+        ls.append(i)
+        ls.append(k)
+print(ls)
